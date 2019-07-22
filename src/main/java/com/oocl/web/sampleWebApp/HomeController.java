76aa6758968136ca35cce5af1306bca611a3712a
suxin <xloypaypa@gmail.com>
2019-07-22 10:55:36 +0800

    1

--- src/main/java/com/oocl/web/sampleWebApp/HomeController.java
+++ src/main/java/com/oocl/web/sampleWebApp/HomeController.java
@@ -3,25 +3,20 @@ package com.oocl.web.sampleWebApp;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public @ResponseBody String greeting() {
         return "Hello World";
     }
 
-    @RequestMapping(value = "/users", method = RequestMethod.POST)
-    public ResponseEntity addUser(@RequestBody String username, HttpServletResponse response) {
-        String id = "1";
-        response.setHeader("Location", "/users/" + id);
+    @PostMapping("/users")
+    public ResponseEntity addUser() {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
